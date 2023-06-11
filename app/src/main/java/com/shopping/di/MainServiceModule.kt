cdda03c87fdc4bcf54b78542bb840be20022c92e
송현서 <s22026@gsm.hs.kr>
2023-06-11 21:07:59 +0900

    Retrofit

---
+++ app/src/main/java/com/shopping/di/MainServiceModule.kt
@@ -0,0 +1,18 @@
+package com.shopping.di
+
+import com.shopping.MainService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MainServiceModule {
+
+    @Provides
+    @Singleton
+    fun providesMainService(retrofit: Retrofit) : MainService = retrofit.create(MainService::class.java)
+}
